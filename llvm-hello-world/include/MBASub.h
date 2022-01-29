192a71b36f35132386d41e7133c321671b324a4c
Haohua Li <masswie@foxmail.com>
2022-01-29 13:36:23 +1100

    refactor with Clion

--- llvm-hello-world/include/MBASub.h
+++ llvm-hello-world/include/MBASub.h
@@ -1,4 +1,8 @@
+//
+// Created by haohua on 29/1/2022.
+//
+
 #ifndef LLVM_TUTOR_MBASUB_H
 #define LLVM_TUTOR_MBASUB_H
 
-
+#endif
---
+++ llvm-hello-world/lib/MBASub.cpp
@@ -0,0 +1,5 @@
+//
+// Created by haohua on 29/1/2022.
+//
+
+#include "MBASub.h"
