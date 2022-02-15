5c7345b978c1c4bbfa669fc55f8b448a9fe709be
Haohua Li <masswie@foxmail.com>
2022-02-15 12:54:27 +1100

    cscd70 assignments

---
+++ cscd70-compiler-optimization/assignment1-intro-llvm/FunctionInfo/test/lit.cfg.py
@@ -0,0 +1,30 @@
+# *lit* is the LLVM integrated tester. Please visit
+# 
+#     https://llvm.org/docs/CommandGuide/lit.html
+# 
+# for its documentation.
+import lit
+import lit.llvm
+
+lit.llvm.initialize(lit_config, config)
+
+from lit.llvm import llvm_config
+
+
+config.name = 'FunctionInfo'
+config.test_format = lit.formats.ShTest()
+                     # The ShTest files contain some number of shell-like
+                     # command pipelines, along with assertions about what
+                     # should be in the output.
+config.test_source_root = "/Users/haohua/Documents/GitHub/cs-nano-projects/cscd70-compiler-optimization/assignment1-intro-llvm/FunctionInfo/test"
+config.test_exec_root   = "/Users/haohua/Documents/GitHub/cs-nano-projects/cscd70-compiler-optimization/assignment1-intro-llvm/FunctionInfo/test"
+config.suffixes = ['.c', '.ll']
+
+config.substitutions.append((r'%dylibdir', "/Users/haohua/Documents/GitHub/cs-nano-projects/cscd70-compiler-optimization/assignment1-intro-llvm/FunctionInfo/lib"))
+
+# Extra config attribute for tool substitutions.
+# FileCheck is not in the default executable path, therefore we need to add it manually.
+config.llvm_config_bindir = ""
+llvm_config.add_tool_substitutions(
+        ["clang", "opt", "FileCheck"],
+        config.llvm_config_bindir)
---
+++ cscd70-compiler-optimization/assignment1-intro-llvm/README.md
@@ -0,0 +1,4 @@
+# Assignment 1 Introduction to LLVM [![handout](https://img.shields.io/badge/-handout-blue)](https://www.overleaf.com/read/nvmnmbntgwqn)
+
+[CSCD70/Assignment1-Introduction_to_LLVM at 2021S · UofT-EcoSystem/CSCD70 (github.com)](https://github.com/UofT-EcoSystem/CSCD70/tree/2021S/Assignment1-Introduction_to_LLVM)
+
