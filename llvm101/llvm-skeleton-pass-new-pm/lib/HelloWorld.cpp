6194b1bb22415c05ad28741348a81637ae105b20
上田光華 <masswie@foxmail.com>
2022-11-15 13:09:53 +0800

    Update HelloWorld.cpp

--- llvm101/llvm-skeleton-pass-new-pm/lib/HelloWorld.cpp
+++ llvm101/llvm-skeleton-pass-new-pm/lib/HelloWorld.cpp
@@ -1,27 +1,85 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
+//=============================================================================
+// FILE:
+//    HelloWorld.cpp
+//
+// DESCRIPTION:
+//    Visits all functions in a module, prints their names and the number of
+//    arguments via stderr. Strictly speaking, this is an analysis pass (i.e.
+//    the functions are not modified). However, in order to keep things simple
+//    there's no 'print' method here (every analysis pass should implement it).
+//
+// USAGE:
+//    1. Legacy PM
+//      opt -enable-new-pm=0 -load libHelloWorld.dylib -legacy-hello-world -disable-output `\`
+//        <input-llvm-file>
+//    2. New PM
+//      opt -load-pass-plugin=libHelloWorld.dylib -passes="hello-world" `\`
+//        -disable-output <input-llvm-file>
+//
+//
+// License: MIT
+//=============================================================================
 #include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
 using namespace llvm;
 
+//-----------------------------------------------------------------------------
+// HelloWorld implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
 namespace {
-  struct HelloWorldPass : public FunctionPass {
-    static char ID;
-    HelloWorldPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "[HelloWorld Pass]: a function called " << F.getName() << "!\n";
-      return false;
-    }
-  };
+// This method implements what the pass does
+void auxRunOnFunction(Function &F) {
+    errs() << "(llvm-tutor) Hello from: "<< F.getName() << "\n";
+    errs() << "(llvm-tutor)   number of arguments: " << F.arg_size() << "\n";
 }
 
-char HelloWorldPass::ID = 0;
-// Or use the lambda function to make things cleanner.... 
-// add the HelloWorldPass to the optimization pipleline 
-//  https://llvm.org/docs/WritingAnLLVMPass.html#basic-code-required
-static RegisterStandardPasses Y(
-    PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM) { PM.add(new HelloWorldPass());}
-  );
+// New PM implementation
+struct HelloWorld : PassInfoMixin<HelloWorld> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &){
+    auxRunOnFunction(F);
+    return PreservedAnalyses::all();
+  }
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired(){ 
+    return true; 
+  }
+};
+
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "hello-world") {
+                    FPM.addPass(HelloWorld());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize HelloWorld when added to the pass pipeline on the
+// command line, i.e. via '-passes=hello-world'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getHelloWorldPluginInfo();
+}
