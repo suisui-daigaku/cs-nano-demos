a1bd0ff2998bfba718889579b7546c4c397256da
上田光華 <masswie@foxmail.com>
2023-01-12 15:48:32 +0800

    signacation

--- c-language/shared_library/sigaction.c
+++ c-language/shared_library/sigaction.c
@@ -1,40 +1,42 @@
-#define _GNU_SOURCE /* Bring REG_XXX names from /usr/include/sys/ucontext.h */
+#define _XOPEN_SOURCE
 
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <stdlib.h> 
 #include <ucontext.h>
 
-static void sigaction_segv(int signal, siginfo_t *si, void *arg)
-{
-    ucontext_t *ctx = (ucontext_t *)arg;
+#define WRFSBASE_LEN 5
 
-    /* We are on linux x86, the returning IP is stored in RIP (64bit) or EIP (32bit).
-       In this example, the length of the offending instruction is 6 bytes.
-       So we skip the offender ! */
-    #if __WORDSIZE == 64
-        printf("Caught SIGSEGV, addr %p, RIP 0x%lx\n", si->si_addr, ctx->uc_mcontext.gregs[REG_RIP]);
-        ctx->uc_mcontext.gregs[REG_RIP] += 6;
-    #else
-        printf("Caught SIGSEGV, addr %p, EIP 0x%x\n", si->si_addr, ctx->uc_mcontext.gregs[REG_EIP]);
-        ctx->uc_mcontext.gregs[REG_EIP] += 6;
-    #endif
+static void sigill_handler(int sig, siginfo_t* si, void* data){
+    // <https://stackoverflow.com/questions/14233464/can-a-c-program-continue-execution-after-a-signal-is-handled>
+    // <https://github.com/hibana-enclave/sgx-lkl/blob/oe_port/src/main-oe/sgxlkl_run_oe.c#L880>
+    ucontext_t* uc = (ucontext_t*) data;
+    printf("[[ signal handler ]]: Illegal instruction handler is here! skip the illegal instrucion to avoid infinite loop \n"); 
+    printf("[[ signal handler ]]: Caught SIGSEGV, addr %p, RIP 0x%llx\n", si->si_addr, uc->uc_mcontext->__ss.__rip); 
+#ifdef __APPLE__
+    uc->uc_mcontext->__ss.__rip += WRFSBASE_LEN;
+#elif __linux__
+    uc->uc_mcontext.gregs[REG_RIP] += RDFSBASE_LEN;
+#else
+    printf("making support for Windows is a nightmare ...\n"); 
+    exit(1); 
+#endif
 }
 
-int main(void)
-{
+int main(void){
     struct sigaction sa;
-
-    memset(&sa, 0, sizeof(sa));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = sigaction_segv;
+    memset(&sa, 0, sizeof(struct sigaction));
     sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGSEGV, &sa, NULL);
-
-    /* Generate a seg fault */
-    *(int *)NULL = 0;
-
-    printf("Back to normal execution.\n");
-
+    sa.sa_sigaction = sigill_handler;
+    if (sigaction(SIGILL, &sa, NULL) == -1){
+        printf("[ERROR] registeration of sigill handler is failed.\n"); 
+        exit(1); 
+    }
+    __asm__ __inline__(
+        "wrfsbase %rax\t\n"
+    );
+    printf("Congradulations!\n"); 
     return 0;
 }
+
