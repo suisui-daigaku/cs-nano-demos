d75950d6cf8df71d2d52033e95eec4aa04812e7d
Kaede Shiina <masswie@foxmail.com>
2023-05-25 11:49:03 +0800

    Update set_affinity.c

--- os/settimer/cpuaffinity/set_affinity.c
+++ os/settimer/cpuaffinity/set_affinity.c
@@ -6,10 +6,34 @@
 */
 
 #include <sched.h>
+#include <stdio.h>
 
+/* Set the CPU affinity for a task */
+extern int sched_setaffinity (__pid_t __pid, size_t __cpusetsize,
+			      const cpu_set_t *__cpuset) __THROW;
+
+/* get cpu id */ 
+extern int sched_getcpu (void) __THROW;
+
+
+# define CPU_ZERO(cpusetp)	 __CPU_ZERO_S (sizeof (cpu_set_t), cpusetp)
+# define CPU_SET(cpu, cpusetp)	 __CPU_SET_S (cpu, sizeof (cpu_set_t), cpusetp)
 
 int main(){
-    int cpu = sched_getcpu(); 
-    
+    int cpu; 
+    cpu_set_t cpuset;
 
+
+    cpu = sched_getcpu(); 
+    printf("Running on CPU %d\n", cpu);
+
+    printf("scheduling the current thread to cpu 3...\n"); 
+    cpu = 3; 
+    CPU_ZERO(&cpuset);
+    CPU_SET(cpu, &cpuset);
+    sched_setaffinity(0, sizeof(cpuset), &cpuset);
+
+    cpu = sched_getcpu(); 
+    printf("Running on CPU %d\n", cpu);
 }
+
